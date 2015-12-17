0a505ee2313aa45b596fe264d24b93f516827af6
michaeljklein <michael14486@gmail.com>
2015-12-16 23:54:09 -0500

    figured out some repa syntax and got constant array functions working

--- Main.hs
+++ Main.hs
@@ -10,14 +10,17 @@ dx (Power c p) = Power (c*p) (p-1)
 
 diff a b = if a < b then b - a else a - b
 
-matDiff a b = R.sumAllP $ a (-^) b
+matDiff a b = R.sumAllP $ a (R.-^) b
 
 okDiff a b = liftM (((1/100)>).abs) $ matDiff a b
 
 centerAt = 64
 
-mId n = "n x n identity matrix"
-mConst n c = "identity matrix * c"
+eq2 (Z :. y :. x) = if x == y then 1 else 0
+eq2c c (Z :. y :. x) = if x == y then c else 0
+
+mId n      = R.fromFunction (ix2 24 24) eq2
+mConst n c = R.fromFunction (ix2 24 24) (eq2c c)
 
 -- | gives m^(a/b) within tolerance defined in 'okDiff'. mr = result matrix, mt = this matrix, mp = m0^(some power), nxn input array
 --   This is an implementation of a matrix fourier series, centered at 'centerAt'.
